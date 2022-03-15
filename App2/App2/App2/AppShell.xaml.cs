ce0f93f6f1db27d57e621db317ef7eeab8a23925
Bexultan <colonialvampire07@mail.com>
2022-03-15 16:51:04 +0600

    00

---
+++ App2/App2/App2/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
