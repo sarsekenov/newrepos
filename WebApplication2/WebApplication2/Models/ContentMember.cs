11eb49d6d4dfdb5d58a280cffb05b75c4b82268b
Bexultan <colonialvampire07@mail.com>
2022-04-13 18:11:42 +0600

    Commonopenfiledialog

---
+++ WebApplication2/WebApplication2/Models/ContentMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ContentMember
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Allows { get; set; }
+        public int ContentId { get; set; }
+        
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/FolderMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class FolderMember
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Allows { get; set; }
+        public string Role { get; set; }
+        public int FolderId { get; set; }
+    }
+}
